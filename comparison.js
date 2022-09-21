b084423be64d7cb5f2ce3037f7d9e2902753f10b
amirulislam26 <26techamirulislam@gmail.com>
2022-09-21 15:53:44 -0700

    added my love javascript comparison values

---
+++ comparison.js
@@ -0,0 +1,31 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(5 != 6);
+// console.log(15 != 15);
+
+//less than or equal
+//consol.log( 5 <= 6);
+//consol.log( 6 <= 6);
+//consol.log( 16 <= 6);
+
+
+//greater than or equal
+// console.log(5 >= 6);
+// console.log(5 >= 2);
+// console.log(2 >= 2);
+
+
+// var num1 = 56;
+// var num2 = 86;
+
+// console.log( num1 < num2);
+// console.log( num1 > num2);
+// console.log( num1 == num2);
+// console.log( num1 != num2);
+// console.log( num1 >= num2);
+// console.log( num1 <= num2);
+
+// multiple conditions
+// money1 > money2 || result1 > result2 || height1 > height2
