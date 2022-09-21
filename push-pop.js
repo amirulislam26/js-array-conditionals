5c8fce834a61a783f0451dad66fd9c36280e465c
amirulislam26 <26techamirulislam@gmail.com>
2022-09-21 15:35:04 -0700

    added a javascript push and pop

--- push-pop.js
+++ push-pop.js
@@ -1,2 +1,17 @@
 var numbers = [78, 45, 98, 45];
+// use push to add element to an array as the last element array
+numbers.push(63);
+// console.log(numbers);
+
+var friends = ['salam','kalam','balam','amir','korim'];
+friends.push('amirul');
+// console.log("Islam");
+
+//use pop to get element
+var element = numbers.push(45);
+// numbers.pop();
 console.log(numbers);
+console.log(element);
+
+
+
