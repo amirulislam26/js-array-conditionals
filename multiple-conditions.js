0dc13b83ff270b0f8e811bde4e7b8ca5e6f42919
amirulislam26 <26techamirulislam@gmail.com>
2022-09-21 16:33:14 -0700

    added a multiple-conditions

---
+++ multiple-conditions.js
@@ -0,0 +1,39 @@
+var isGraduated = true;
+var mySalary = 75000;
+var cars = 1;
+
+// if(isGraduated == true){
+//     console.log('eso love kori');
+// }
+// else{
+//     console.log('tor love er dorkar nai');
+// }
+
+
+// if(isGraduated == true && mySalary > 50000){
+//     console.log('eso love kori');
+// }
+// else{
+//     console.log('tor love er dorkar nai');
+// }
+
+// if(isGraduated == true && mySalary > 50000 && cars >= 1){
+//     console.log('eso love kori');
+// }
+// else{
+//     console.log('tor love er dorkar nai');
+// }
+
+// if(isGraduated == true || mySalary > 50000){
+//     console.log('eso love kori');
+// }
+// else{
+//     console.log('tor love er dorkar nai');
+// }
+
+if(isGraduated == true && (mySalary > 50000 || cars >= 1)){
+    console.log('eso love kori');
+}
+else{
+    console.log('tor love er dorkar nai');
+}
