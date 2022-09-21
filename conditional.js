aed69d95623abdce63744f1bbc0796b63b6fc141
amirulislam26 <26techamirulislam@gmail.com>
2022-09-21 16:13:11 -0700

    added a new conditional

---
+++ conditional.js
@@ -0,0 +1,34 @@
+var iphonePrice = 79000;
+var myBudget = 9500;
+
+// if iphone price is less then my budget. l will buy the iphone
+/*
+*if iphone price is less then my budget
+*i will buy the iphone
+*
+*if (condition) {
+    //will execute if the condition is true
+}
+*
+*/
+
+if (iphonePrice < myBudget) {
+    console.log('i phone kinbo and style dekhaibo tate top bab er ki')    
+}
+
+var chickenPrice = 65;
+var myBudget = 50;
+if (chickenPrice <= myBudget) {
+    console.log('murger raan khamu and chibamu');
+}
+if (chickenPrice > myBudget){
+    console.log('smashed potato and lentils soup');
+}
+
+//--------------------------------//
+if (chickenPrice < myBudget) {
+    console.log('i will eat chicken');
+}
+else{
+    console.log('i will eat potato and lentils soup');
+}
