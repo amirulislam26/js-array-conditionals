795ade8c75e7a651a1547bdb4d87d3c880a2208f
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 20:02:40 -0700

    added my javascript multi-stage-conditions

---
+++ multi-stage-conditions.js
@@ -0,0 +1,14 @@
+var money = 40;
+var danisPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+if(danisPrice < money){
+    console.log('dan Dan danish khamu');
+}
+else if (butterBread < money) {
+    console.log('butter bon khamu')
+}
+else if (toastBiscuit < money){
+    console.log('toast biskit khamu')
+}
+else console.log('khali cha ei sasther jonno valo')
