0b9bf43711fae0f7d4ec2f358c03296848ab441e
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 21:49:24 -0700

    added a my practices and provlem solved and my summary

---
+++ summary.js
@@ -0,0 +1,23 @@
+var destinations = ['sajek', 'amir', 'monir','bali', 'sajek', 'gonir'];
+
+console.log(destinations[2])
+    destinations[2] = 'thailend';
+    var index = destinations.indexOf('bali');
+    destinations.push('bhutan');
+    destinations.pop()
+
+    //conditionals 
+    // <, >, ==, <=, >= 
+    // &&, ||
+    if(destinations[1] === 'sajek'){
+
+    }
+    else if (destinations[2] === 'bali'){
+
+    }
+    else if (destinations.length === 3 ){
+
+    }
+    else{
+
+    }
