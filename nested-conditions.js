5dc21c3ccef3aac6369a2cdb01de083115db19ce
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 20:11:32 -0700

    added my nested-conditions

---
+++ nested-conditions.js
@@ -0,0 +1,20 @@
+var math = true;
+var geometry = true;
+var strightLint = false;
+
+if(math == true){
+    if(geometry == true) {
+if(strightLint == true){
+
+}
+else{
+    console.log('baka pothe cholba na')
+}
+    }
+    else{
+        console.log('pithagorous hoite parba na')
+    }
+}
+else{
+
+}
